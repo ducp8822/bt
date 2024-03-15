09efcd243ede43fcca4bc85e00090254e6c8c340
ducp8822 <ducp8822@gmail.com>
2024-03-15 11:09:42 +0700

    Update bai1.cpp

--- mang_va_cstring/bai1.cpp
+++ mang_va_cstring/bai1.cpp
@@ -1,11 +1,7 @@
-Viết chương trình nhận input từ bàn phím gồm một cặp số m, n nguyên dương nhỏ hơn 10 và một bản đồ mìn dạng bảng, * nghĩa là có mìn, . là không có mìn, các kí tự cách nhau bởi 01 space. In ra màn hình một bảng m dòng n cột sao cho giá trị của mỗi ô là một dấu * nếu như ở đó có mìn, nếu không thì là một giá trị trong khoảng 0...8 là số mìn nằm trong các ô xung quanh. Ví dụ
-Sample Input
-Sample Output
-3 5
-* * . . .
-. . . . .
-. * . . .
-* * 1 0 0
-3 3 2 0 0
-1 * 1 0 0
+#include <bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    
+}
