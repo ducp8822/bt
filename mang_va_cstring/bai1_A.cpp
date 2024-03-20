470ad37d668d8790c584ad2e91d225d5e75c56fe
ducp8822 <ducp8822@gmail.com>
2024-03-20 10:11:27 +0700

    Create bai1_A.cpp

---
+++ mang_va_cstring/bai1_A.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// int a[];
+int a2[4]={1,2,3,4};
+int a4[]={1,2,3,4};
+
+int main()
+{
+    // int a[];
+    int a3[]={1,2,3,4};
+    int a1[4]={1,2,3,4};
+    for (int i=0;i<4;i++){
+        cout<<a2[i]<<" ";
+    }
+    for (int i=0;i<4;i++){
+        cout<<a1[i]<<" ";
+    }
+    for (int i=0;i<4;i++){
+        cout<<a3[i]<<" ";
+    }for (int i=0;i<4;i++){
+        cout<<a4[i]<<" ";
+    }
+}
