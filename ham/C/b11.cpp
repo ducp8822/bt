697d0f93669f73f91a0a0870b6ef5cdbb6d73987
ducp8822 <ducp8822@gmail.com>
2024-04-22 16:36:48 +0700

    H

--- ham/C/b11.cpp
+++ ham/C/b11.cpp
@@ -1,7 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector <int> DeToBi(int x)
+{
+    vector <int> v;
+    while(x!=0){
+        int k=x%10;
+        x=x/10;
+        v.push_back(k);
+    }
+    return v;
+}
+
 int main()
 {
-    
+    int x;
+    vector <int> v =DeToBi(x);
+    for (auto x:v){
+        cout<<x;
+    }
 }
